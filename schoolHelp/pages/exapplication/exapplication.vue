918b99f35db57836b2b194fb027c3e712cf34dad
Funny-Panda <41933860+Funny-Panda@users.noreply.github.com>
2019-04-27 17:01:23 +0800

    首页、应用、我的初步完成

---
+++ schoolHelp/pages/exapplication/exapplication.vue
@@ -0,0 +1,181 @@
+<template>
+	<view>
+		<html>
+			<body>
+				<div class="wrapper">
+					<div class="box-wrap">
+						<div class="box-wrap-left" @click="gotoQuestionaire()">
+							<image class="exappImage" src="../../static/wenjuandiaocha.png" />
+							<p>问卷调查</p>
+						</div>
+						<div class="box-wrap-center">
+							<image class="exappImage" src="../../static/kechengbiao.png" />
+							<p>课程表</p>
+						</div>
+						<div class="box-wrap-right">
+							<image class="exappImage" src="../../static/kongjiaoshi.png" />
+							<p>空教室</p>
+						</div>
+					</div>
+					<div class="box-wrap">
+						<div class="box-wrap-left">
+							<image class="exappImage" src="../../static/zidongpingyi.png" />
+							<p>自动评议</p>
+						</div>
+						<div class="box-wrap-center">
+							<image class="exappImage" src="../../static/kaoshianpai.png" />
+							<p>考试安排</p>
+						</div>
+						<div class="box-wrap-right">
+							<image class="exappImage" src="../../static/kandianpu.png" />
+							<p>看店铺</p>
+						</div>
+					</div>
+					<div class="box-wrap">
+						<div class="box-wrap-left">
+							<image class="exappImage" src="../../static/biaobaiqiang.png" />
+							<p>表白墙</p>
+						</div>
+					</div>
+					<div class="push"></div>
+				</div>
+				<footer class="footer">
+					<view class="page-section swiper">
+						<view class="page-section-spacing">
+							<swiper class="swiper" :indicator-dots="indicatorDots" :autoplay="autoplay" :interval="interval" :duration="duration">
+								<swiper-item>
+									<image src="../../static/add1.png" class="adds"></image>
+								</swiper-item>
+								<swiper-item>
+									<image src="../../static/add2.png" class="adds"></image>
+								</swiper-item>
+								<swiper-item>
+									<image src="../../static/add3.png" class="adds"></image>
+								</swiper-item>
+							</swiper>
+						</view>
+					</view>
+				</footer>
+			</body>
+		</html>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				indicatorDots: true,
+				autoplay: true,
+				interval: 2000,
+				duration: 500
+			}
+		},
+		methods: {
+			gotoQuestionaire() {
+				uni.navigateTo({
+					url: '/pages/exapplication/questionnaire/questionnaire'
+				});
+			}
+		}
+	}
+</script>
+
+<style>
+	.wrapper {
+		min-height: 100%;
+		margin-bottom: 70px;
+	}
+
+	body,
+	html {
+		height: 100%;
+		margin: 0;
+	}
+
+	.footer,
+	.push {
+		height: 70px;
+	}
+
+	.swiper {
+		height: 300upx;
+	}
+
+	.adds {
+		width: 100%;
+		height: 100%;
+	}
+
+	.exappImage {
+		text-align: center;
+		border-radius: 30px;
+		margin-top: 2px;
+		width: 60px;
+		height: 60px;
+		max-width: 100%;
+		max-height: 100%;
+	}
+
+	.box-wrap {
+		/* flex-direction: row; */
+		width: 360px;
+		text-align: center;
+		background-color: #FFFFFF;
+		overflow: hidden;
+		/* justify-content: center; */
+	}
+
+	.box-wrap-left {
+		float: left;
+		border: 1px solid #CCCCCC;
+		width: 100px;
+		height: 100px;
+		/* width: 30%;
+		padding-bottom: 25%; */
+		margin-left: 10px;
+		margin-top: 5px;
+		/* margin: 0 auto; */
+		border-radius: 10%;
+		background-color: FFFFFF;
+	}
+
+	.box-wrap-left:active {
+		background-color: #eeeeee;
+	}
+
+	.box-wrap-center {
+		float: left;
+		border: 1px solid #CCCCCC;
+		width: 100px;
+		height: 100px;
+		/* padding-bottom: 25%; */
+		margin-top: 5px;
+		margin-left: 17px;
+		/* margin: 0 auto; */
+		border-radius: 10%;
+		background-color: FFFFFF;
+	}
+
+	.box-wrap-center:active {
+		background-color: #eeeeee;
+	}
+
+	.box-wrap-right {
+		/* display: inline; */
+		float: right;
+		border: 1px solid #CCCCCC;
+		width: 100px;
+		height: 100px;
+		/* width: 30%;
+		padding-bottom: 25%; */
+		margin-right: 10px;
+		margin-top: 5px;
+		border-radius: 10%;
+		background-color: FFFFFF;
+	}
+
+	.box-wrap-right:active {
+		background-color: #eeeeee;
+	}
+</style>
---
+++ schoolHelp/pages/ucenter/ucenter.vue
@@ -0,0 +1,212 @@
+<template>
+	<view class="center">
+		<view class="logo" @click="goLogin" :hover-class="!login ? 'logo-hover' : ''">
+			<image class="logo-img" :src="login ? uerInfo.avatarUrl :avatarUrl"></image>
+			<view class="logo-title">
+				<text class="uer-name">Hi，{{login ? uerInfo.name : '您未登录'}}</text>
+				<text class="go-login navigat-arrow" v-if="!login">&#xe65e;</text>
+			</view>
+		</view>
+		<view>
+			<div class="basicData">
+				<div class="basicDataItem"><text>0</text><text>我的关注</text>
+				</div>
+				<div class="basicDataItem"><text>0</text><text>我的收藏</text>
+				</div>
+				<div class="basicDataItem"><text>0</text><text>我的帖子</text>
+				</div>
+				<div class="basicDataItem"><text>0</text><text>我的评论</text>
+				</div>
+				<div class="basicDataItem"><text>0</text><text>我的积分</text>
+				</div>
+			</div>
+		</view>
+		<view class="center-list">
+			<view class="center-list-item border-bottom">
+				<text class="list-icon">&#xe60f;</text>
+				<text class="list-text">设置</text>
+				<text class="navigat-arrow">&#xe65e;</text>
+			</view>
+			<view class="center-list-item border-bottom">
+				<text class="list-icon">&#xe60b;</text>
+				<text class="list-text">修改资料</text>
+				<text class="navigat-arrow">&#xe65e;</text>
+			</view>
+			<view class="center-list-item border-bottom">
+				<text class="list-icon">&#xe639;</text>
+				<text class="list-text">意见反馈</text>
+				<text class="navigat-arrow">&#xe65e;</text>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				login: true,
+				avatarUrl: '/static/logo.png',
+				uerInfo: {}
+			}
+		},
+		methods: {
+			goLogin() {
+				if (!this.login) {
+					console.log('点击前往登录');
+					uni.navigateTo({
+						url: 'login/login'
+					})
+				};
+			}
+		}
+	}
+</script>
+
+<style>
+	@font-face {
+		font-family: texticons;
+		font-weight: normal;
+		font-style: normal;
+		src: url('https://at.alicdn.com/t/font_984210_5cs13ndgqsn.ttf') format('truetype');
+	}
+
+	page,
+	view {
+		display: flex;
+	}
+
+	page {
+		background-color: #f8f8f8;
+	}
+
+	.basicData {
+		width: 100%;
+		margin: 5px;
+		display: flex;
+		flex-direction: row;
+		flex-wrap: nowrap;
+		background-color: #FFFFFF;
+		justify-content: space-between;
+	}
+
+	.basicDataItem {
+		width: 60px;
+		font-size: 80%;
+		text-align: center;
+		display: flex;
+		flex-direction: column;
+		flex-wrap: nowrap;
+		justify-content: space-between;
+		background-color: #f2f2f2;
+		/* border: 1px solid #CCCCCC; */
+	}
+
+	.center {
+		flex-direction: column;
+	}
+
+	.logo {
+		width: 750upx;
+		height: 240upx;
+		padding: 20upx;
+		box-sizing: border-box;
+		background-color: #2F85FC;
+		flex-direction: row;
+		align-items: center;
+	}
+
+	.logo-hover {
+		opacity: 0.8;
+	}
+
+	.logo-img {
+		width: 150upx;
+		height: 150upx;
+		border-radius: 150upx;
+	}
+
+	.logo-title {
+		height: 150upx;
+		flex: 1;
+		align-items: center;
+		justify-content: space-between;
+		flex-direction: row;
+		margin-left: 20upx;
+	}
+
+	.uer-name {
+		height: 60upx;
+		line-height: 60upx;
+		font-size: 38upx;
+		color: #FFFFFF;
+	}
+
+	.go-login.navigat-arrow {
+		font-size: 38upx;
+		color: #FFFFFF;
+	}
+
+	.login-title {
+		height: 150upx;
+		align-items: self-start;
+		justify-content: center;
+		flex-direction: column;
+		margin-left: 20upx;
+	}
+
+	.center-list {
+		background-color: #FFFFFF;
+		margin-top: 20upx;
+		width: 750upx;
+		flex-direction: column;
+	}
+
+	.center-list-item {
+		height: 90upx;
+		width: 750upx;
+		box-sizing: border-box;
+		flex-direction: row;
+		padding: 0upx 20upx;
+	}
+
+	.center-list-item:active {
+		background-color: #eeeeee;
+	}
+
+	.border-bottom {
+		border-bottom-width: 1upx;
+		border-color: #c8c7cc;
+		border-bottom-style: solid;
+	}
+
+	.list-icon {
+		width: 40upx;
+		height: 90upx;
+		line-height: 90upx;
+		font-size: 34upx;
+		color: #2F85FC;
+		text-align: center;
+		font-family: texticons;
+		margin-right: 20upx;
+	}
+
+	.list-text {
+		height: 90upx;
+		line-height: 90upx;
+		font-size: 34upx;
+		color: #555;
+		flex: 1;
+		text-align: left;
+	}
+
+	.navigat-arrow {
+		height: 90upx;
+		width: 40upx;
+		line-height: 90upx;
+		font-size: 34upx;
+		color: #555;
+		text-align: right;
+		font-family: texticons;
+	}
+</style>
